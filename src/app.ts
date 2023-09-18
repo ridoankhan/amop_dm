ed8173fd703cb3e9de15b9daed3b12e63c0a96fc
Ridoan Khan Anik <ridoankhan07@gmail.com>
2023-09-18 14:49:17 +0600

    added colorette for coloring server log texts and used @fastify/cors for handling cors from cross origin

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import { databaseConfig } from './config/database.config'
 import { inventoryRoutes } from './modules/inventory/inventory.route'
 import errorHandlerPlugin from './middlewares/error.middleware'
+import cors from '@fastify/cors'
+import { blue, yellow, red, bold, underline } from 'colorette'
 
 const fastify = Fastify({
   logger: true,
@@ -10,11 +12,17 @@ const fastify = Fastify({
 
 mongoose
   .connect(databaseConfig.url)
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => console.log(underline(bold(yellow('Connected to MongoDB')))))
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err)
+    console.error(red('Error connecting to MongoDB:'), err)
     process.exit(1)
   })
+
+fastify.register(cors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true,
+})
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
@@ -36,7 +44,9 @@ const start = async () => {
       },
       (err, address) => {
         if (err) throw err
-        console.log(`Server listening on ${address}`)
+        console.log(
+          underline(blue(bold(`Listening on: http://localhost:3000`)))
+        )
       }
     )
   } catch (err) {
